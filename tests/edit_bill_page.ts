ef9e9b8819ee463eae4bd12a9e866506297bd957
Jesper <jesper@flygar.se>
2024-09-26 00:27:41 +0200

    checked working for bill

--- tests/edit_bill_page.ts
+++ tests/edit_bill_page.ts
@@ -3,6 +3,7 @@ import { expect, type Locator, type Page } from '@playwright/test';
 export class EditBillPage 
 {
     readonly page: Page;
+    variableIsChecked: Boolean;
 
     constructor(page: Page) 
     {
@@ -17,12 +18,27 @@ export class EditBillPage
     async isChecked(optionToEdit: Number): Promise<Boolean>
     {
         let paidText: Locator = this.page.locator('#app > div > div.bills > div:nth-child('+ optionToEdit +') > div.paid');
+        console.log('this1:::::' + paidText); 
         const checked = paidText.innerText();
-        if(checked == new Promise<String>(() => 'Yes'))
+        console.log('this2:::::' + checked);
+
+
+        //this.variableIsChecked = false;
+        //await checked.then(val => //then wait for value of promise object checked
+        //{
+        if(await checked == 'Paid: Yes')
         {
-            return new Promise<Boolean>(() => true);
+            console.log('hi');
+            return true; 
+            //this.variableIsChecked = true;
+            // return new Promise<Boolean>(() => true);
         }
-        return new Promise<Boolean>(() => false);
+        // console.log('hi2'); 
+        // })
+        console.log('hi33'); 
+        return false; 
+        // console.log('hi44' + val);  
+        // return val; 
     }
 
     async preformEditBill(optionToEdit: Number, value: Number) 
